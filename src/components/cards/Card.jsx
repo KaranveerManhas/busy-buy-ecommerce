540aeff54be70e904b85345b52f803b04cc140a2
Karanveer Singh <82330083+KaranveerManhas@users.noreply.github.com>
2024-05-02 19:58:05 +0530

    Add styling to card component and fetch product data from API

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,18 +1,42 @@
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
-export const CardComponent = () => {
+
+export const CardComponent = ({product}) => {
   return (
-    <Card style={{ width: '18rem' }} className="bg-body-tertiary">
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div className="d-flex justify-content-center flex-wrap rounded-2 p-4 shadow bg-body-tertiary" style={styles.card}>
+      <div style={styles.imageContainer} className="d-flex align-items-center justify-content-center">
+        <img src={product.image} alt={product.title} style={styles.image} />
+      </div>
+      <div style={styles.infoDiv} className="d-flex flex-column justify-content-end">
+        <h5 style={styles.title} >{product.title}</h5>
+        <h5 className="fw-bold">$ {product.price}</h5>
+        <Button variant="primary">Add to Cart</Button>
+      </div>
+    </div>
   );
+}
+
+const styles = {
+  card: {
+    width: '250px',
+    height: "500px",
+  },
+  imageContainer: {
+    width: "100%",
+    height: "70%",
+  },
+  image: {
+    width: "170px",
+  },
+  infoDiv: {
+    height: "30%",
+    alignSelf: "flex-end"
+  },
+  title: {
+    display: "-webkit-box",
+    "-webkit-line-clamp": "2",
+    "-webkit-box-orient": "vertical",
+    overflow: "hidden",
+    textOverflow: "ellipsis"
+  }
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,51 @@
 import Container from "react-bootstrap/esm/Container";
 import { CardComponent } from "../../components/cards/Card";
+import { useEffect, useState } from "react";
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+import { GridLoader } from 'react-spinners';
 
 
+const override = {
+    display: 'block',
+    margin: '100px auto 0'
+}
 
 export const Home = () => {
+
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("https://fakestoreapi.com/products");
+            const data = await response.json();
+            setProducts(data);
+        }
+        fetchData();
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+
+    }, [])
+
     return (
-        <Container className="d-flex justify-content-center border mt-5 p-3 ">
-            <CardComponent />
-        </Container>
+        <div style={{height: "85vh"}}>
+            <Container className="d-flex flex-wrap justify-content-evenly gap-4 mt-5 p-3 h-100">
+                {isLoading ? 
+                <GridLoader 
+                    color="#ff9a01"
+                    loading={isLoading}
+                    cssOverride={override}
+                    size={20}
+                    aria-label="Loading Products"
+                    data-testid="loader"
+                />
+                 : 
+                products.map((product) => <CardComponent product={product} />)}
+            </Container>
+        </div>
+        
     )
 }
